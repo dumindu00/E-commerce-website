a29ee6ed64ef6931be6816ec806b21dc85187d2d
dumindu00 <duminduviraj26@gmail.com>
2026-01-07 22:08:00 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose"); // help you to connect server with database
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const mongo_url = process.env.DB_URL;
+
+mongoose.connect(mongo_url
+).then(() => console.log("MongoDB connected"))  // return a promise once promise resolved that means connected server with database
+.catch((error) => console.log(error))
+
+
+const app = express() // create app
+const PORT = process.env.PORT || 5000;
+
+
+app.use(
+    cors({
+        origin : 'http://localhost:5173/',
+        methods : ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders : [
+            "Content-Type",
+            "Authorization",
+            "Cache-Control",
+            "Expires",
+            "Pragma"
+        ],
+        credentials : true
+        
+    })
+)
+
+
+app.use(cookieParser())
+app.use(express.json())
+
+app.listen(PORT, ()=> console.log(`Server Running😎 on PORT${PORT}`))
