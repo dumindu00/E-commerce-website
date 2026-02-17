b17a589e822ff9d7eedfce25caf3c0816e943638
Your Name <you@example.com>
2026-02-17 22:28:48 +0530

    auth-features-ui

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -11,7 +11,33 @@ export const registerFormControls = [
         name : 'email',
         label: 'User Email',
         placeholder: 'Enter your Email',
-        componentType: 'Email',
+        componentType: 'input',
+        type: 'email',
+    },
+    {
+        name : 'password',
+        label: 'Password',
+        placeholder: 'Enter your password',
+        componentType: 'input',
+        type: 'password',
+    }
+]
+
+
+export const loginFormControls = [
+    
+    {
+        name : 'userName',
+        label: 'User Name',
+        placeholder: 'Enter your name',
+        componentType: 'input',
+        type: 'text',
+    },
+    {
+        name : 'email',
+        label: 'User Email',
+        placeholder: 'Enter your Email',
+        componentType: 'input',
         type: 'email',
     },
     {
