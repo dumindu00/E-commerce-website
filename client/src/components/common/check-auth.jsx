a137267dbecdbd787b13a02e475e8068e90dc67c
Your Name <you@example.com>
2026-02-20 21:06:41 +0530

    login-route-bug-fix

--- client/src/components/common/check-auth.jsx
+++ client/src/components/common/check-auth.jsx
@@ -27,7 +27,7 @@ function CheckAuth ({isAuthenticated, user, children}) {
             if(user?.role === 'admin') {
                 return <Navigate to="/admin/dashboard" />
             } else {
-                return <Navigate to="/shop/dashboard" />
+                return <Navigate to="/shop/home" />
             }
         }
 
