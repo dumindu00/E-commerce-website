d54314f23f1d6442818f03b6d4f8554ae377042c
Your Name <you@example.com>
2026-04-09 18:36:34 +0530

    filters-improvement

--- client/src/components/shopping-view/product-tile.jsx
+++ client/src/components/shopping-view/product-tile.jsx
@@ -1,6 +1,7 @@
+import { brandOptionsMap, categoryOptionsMap } from "@/config";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
-import { Card, CardFooter } from "../ui/card";
+import { Card, CardContent, CardFooter } from "../ui/card";
 
 
 
@@ -21,16 +22,16 @@ function ShoppingProductTile({product}) {
                         }
                     </div>
                     <CardContent className="p-4">
-                        <h2 className="text-xl font-bold mb-2">{product?.tile}</h2>
+                        <h2 className="text-xl font-bold mb-2">{product?.title}</h2>
                         <div className="flex justify-between items-center mb-2">
-                            <span className="text-sm text-muted-foreground">{product?.category}</span>
-                            <span className="text-sm text-muted-foreground">{product?.brand}</span>
+                            <span className="text-sm text-muted-foreground">{categoryOptionsMap[product?.category]}</span>
+                            <span className="text-sm text-muted-foreground">{brandOptionsMap[product?.brand]}</span>
                         </div>
                         <div className="flex justify-between items-center mb-2">
-                            <span className={`${product?.salePrice > 0 ? 'line-through' : '' } text-lg font-semibold text-primary`} >{product?.price}</span>
+                            <span className={`${product?.salePrice > 0 ? 'line-through' : '' } text-lg font-semibold text-primary`} >${product?.price}</span>
                                 {
                                     product?.salePrice > 0 ? 
-                                    <span className="text-lg font-semibold text-primary">{product?.salePrice}</span>
+                                    <span className="text-lg font-semibold text-primary">${product?.salePrice}</span>
                                     : null
                                 }
                             
